0ad27f4880270dc0ea014392d20cb971155eeaec
Jonathan Claypool <jonathanclaypool@Jonathans-MacBook-Pro.local>
2017-03-08 09:52:04 -0600

    I am adding the code for the first time on Github.
    This is the first commit to the repository set up by me. I am the ruler of the
    universe and as such have ultimate authority to use whatever fruits I want in
    my famous celestial fruit salad.

---
+++ showDadCode.py
@@ -0,0 +1,12 @@
+familyMembers = ['emma', 'jonathan', 'david', 'maria', 'olivia']
+
+print(familyMembers)
+
+sentence = "Today at the wedding I saw emma, jonathan, and david. Later I saw olivia and maria."
+
+print(sentence)
+
+for i in range(len(familyMembers)):
+    sentence = sentence.replace(familyMembers[i], (familyMembers[i] + ' Claypool'))
+
+print(sentence)
